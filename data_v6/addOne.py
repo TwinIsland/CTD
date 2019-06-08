d3cd3ea30420dd13d43c9411a04ab02cb007bc20
Wyatt <p@hty.email>
2019-06-08 20:55:23 +0800

    modified database

---
+++ data_v6/addOne.py
@@ -0,0 +1,12 @@
+#encoding:utf-8
+
+import library
+
+
+library = library.library()
+
+
+library.addMethod([54764,'金庸作品全集','金庸','现代','金庸作品全集，明河社出版','https://ctd-1257758577.cos.ap-guangzhou.myqcloud.com/data/%E5%85%B6%E4%BB%96/%E9%87%91%E5%BA%B8%E4%BD%9C%E5%93%81%E5%85%A8%E9%9B%86.txt'],'download')
+
+
+library.save_change()
